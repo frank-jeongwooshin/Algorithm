93156edeea9d355e2a905da15cb076dd8780b7f2
frank-jeongwooshin <maecsan12@gmail.com>
2022-01-04 23:27:44 +0900

    update_220104

---
+++ C++/PracticeCpp/pro_220104.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+// Ã¼À°º¹
+class trainingClothes
+{
+public:
+    int solution(int n, vector<int> lost, vector<int> reserve);
+};
+
